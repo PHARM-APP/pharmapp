be336783adbfd95eb88625c7863c476a992d76e4
marwen <marwenkhorchani7@gmail.com>
2020-12-21 17:13:56 +0100

    apl image fixed

--- myapp/src/app/navbar/navbar.component.ts
+++ myapp/src/app/navbar/navbar.component.ts
@@ -1,17 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import {DetailsService} from '../services/details.service'
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
- 
+  myArray:any =[]
   isLoggedIn = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,productService:DetailsService, private service:DetailsService) { 
+    productService.getproduct().subscribe((res)=>{
+      this.myArray=res  })
+        this.loadAllProducts()
+  }
+  loadAllProducts(){
+    this.service.getAllProducts().subscribe(response=>{
+      console.log(response)
+      this.myArray=response
+    })
+  }
+  deleteProduct(id:number){
+    console.log(id)
+  
+    this.service.delete(id)
+  
+     .subscribe(()=>{
+      // return this.myArray=this.myArray.filter((item:any)=>item.id !== id)
+      return   this.loadAllProducts()
 
+     })
+   }
 
   onlogout() {
     this.isLoggedIn = false;
@@ -20,6 +40,7 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    
   }
 
 }
